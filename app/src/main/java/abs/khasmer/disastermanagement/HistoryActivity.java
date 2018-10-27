51bf6e024e3bcbaf913644baecf25f51924c455e
sharafzaman <sharafzaz121@gmail.com>
2018-10-27 21:04:48 +0530

    ui improvements

--- app/src/main/java/abs/khasmer/disastermanagement/HistoryActivity.java
+++ app/src/main/java/abs/khasmer/disastermanagement/HistoryActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 
 /*
  * Created by sh_zam on 27/10/18
@@ -17,10 +18,21 @@ public class HistoryActivity extends AppCompatActivity {
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.history_activity);
+		if (getSupportActionBar() == null) {
+			// theme changed
+			return;
+		}
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 		RecyclerView recyclerView = findViewById(R.id.recyclerView);
 		recyclerView.setLayoutManager(new LinearLayoutManager(this,
 				LinearLayoutManager.VERTICAL, false));
 		recyclerView.setAdapter(new HistoryAdapter(this, new DataSaver(this).getResults()));
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		onBackPressed();
+		return super.onOptionsItemSelected(item);
 	}
 }
